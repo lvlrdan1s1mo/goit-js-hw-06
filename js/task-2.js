524b7553a9ba8d229d01b197007f74fe1b7b0aa2
lvlrdan1s1mo <danyamalaev1@gmail.com>
2025-03-05 15:32:37 +0100

    Add files via upload

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,41 @@
 class Storage {
+  #items;
+
   constructor(initialItems) {
-    this.items = initialItems;
+    this.#items = initialItems;
   }
 
   getItems() {
-    return this.items;
+    console.log("Текущие элементы:", this.#items);
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
+    console.log(`Добавлен элемент: ${newItem}`);
+    console.log("Новый список:", this.#items);
   }
 
   removeItem(itemToRemove) {
-    const index = this.items.indexOf(itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
     if (index !== -1) {
-      this.items.splice(index, 1);
+      this.#items.splice(index, 1);
+      console.log(`Удален элемент: ${itemToRemove}`);
+    } else {
+      console.log(`Элемент "${itemToRemove}" не найден.`);
     }
+    console.log("Текущий список после удаления:", this.#items);
   }
 }
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
